bcb9df9333e47295e88b730d80b83c61c47da559
jakelevi1996 <jakelevi@hotmail.co.uk>
2018-10-24 22:06:26 +0100

    Predict variance of GP and plot

--- code/inference.py
+++ code/inference.py
@@ -4,25 +4,51 @@ import matplotlib.pyplot as plt
 from models.gaussianprocess import GaussianProcess
 from data.data import load_xy, plot_1D
 
+def plot_predictions(
+    x_train, y_train, x_test,
+    mean, std, std_scale=1.96,
+    filename="code/results/sinusoid_predictions"
+):
+    plt.figure()
+    plt.plot(x_train, y_train, 'rx')
+    plt.plot(x_test, mean, 'b-')
+    plt.fill_between(
+        x_test,
+        (mean + std_scale*std).reshape(-1),
+        (mean - std_scale*std).reshape(-1),
+        alpha=0.2
+    )
+    plt.grid(True)
+    plt.savefig(filename)
+    plt.close()
+
+def gaussian_density(x, mean, std):
+    return np.exp(
+        -0.5 * np.square((x - mean) / std)
+    ) / np.sqrt(2 * np.pi * std)
+
+def plot_density(
+    x_train, y_train, x_test,
+    mean, std,
+    filename="code/results/sinusoid_predictions"
+):
+    pass
+
 if __name__ == "__main__":
     x_train, y_train = load_xy("code/data/sinusoidal_data.npz")
     plot_1D(x_train, y_train, format_str='ro')
 
-    # length = np.exp(-1)
-    # log_length = np.log(length)
     log_length = -1
-    log_std = -10
+    log_scale = 2
+    log_std = -5
 
     gp = GaussianProcess(
         x_train, y_train,
-        log_length=log_length, log_std=log_std
+        log_length=log_length, log_scale=log_scale, log_std=log_std
     )
-    x_test = np.linspace(min(x_train), max(x_train))
-    y_predict = gp.predict_mean(x_test)
 
-    plt.figure()
-    plt.plot(x_train, y_train, 'bx')
-    plt.plot(x_test, y_predict, 'r--')
-    plt.grid(True)
-    plt.savefig("code/results/sinusoid_predictions")
-    plt.close()
+    x_test = np.linspace(0, 1, 200)
+    mean, std = gp.predict(x_test)
+
+    plot_predictions(x_train, y_train, x_test, mean, std)
+    
--- code/models/gaussianprocess.py
+++ code/models/gaussianprocess.py
@@ -1,27 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def se_iso_cov(x1, x2, log_length=-1, log_std=0):
+def se_iso_cov(x1, x2, log_length=-1, log_scale=0, log_std=0):
     """
     ***NOT SET UP TO COPE WITH MULTIDIMENSIONAL INPUTS.***
     np.square must be replaced with the L2 norm, and reshaping must be able to
     handle 1D arrays as well as 2D matrices
     """
     length = np.exp(log_length)
+    scale = np.exp(log_scale)
     std = np.exp(log_std)
-    # std = 0
 
     # x1 and x2 should be `np.array`s
     if type(x1) is not np.ndarray: x1 = np.array(x1)
     if type(x2) is not np.ndarray: x2 = np.array(x2)
 
-    # x1 and x2 should be 1D vectors or 2D matrices
-    # TODO: update for multi-D inputs!
+    # If x1 and x2 are not matrices, assume they're column vectors
     if not len(x1.shape) == 2: x1 = x1.reshape(-1, 1)
     if not len(x2.shape) == 2: x2 = x2.reshape(-1, 1)
 
-    # Component from nearby locations:    
-    k = np.exp(
+    # Calculate covariances:    
+    k = scale * np.exp(
         -0.5 * np.square((x1 - x2.T) / length)
     ) + std * np.equal(x1, x2.T)
 
@@ -33,41 +32,61 @@ class GaussianProcess():
         x_train, y_train,
         cov_func=se_iso_cov,
         log_length=-1,
-        log_std=0
-
+        log_scale=0,
+        log_std=0,
     ):
         # x_train should be a 1D vector or 2D matrix
         if type(x_train) is not np.ndarray: x_train = np.array(x_train)
         if not len(x_train.shape) == 2: x_train = x_train.reshape(-1, 1)
         self.x_train = x_train
         
-        # Targets, used for predicting means
+        # Targets, used for predicting means:
         self.targets = y_train.reshape(-1, 1)
-        self.kernel = cov_func
+        # Covariance function and hyperparameters:
+        self.cov_func = cov_func
         self.log_length = log_length
+        self.log_scale = log_scale
         self.log_std = log_std
         # Covariance of the training inputs, using the given kernel:
-        self.cov_train = self.kernel(
-            x_train, x_train,
-            log_length=self.log_length, log_std=self.log_std
-        )
-        print(self.cov_train)
+        self.cov_train = self.kernel(x_train, x_train)
         # Only invert this when it's needed:
         self.cov_train_inv = None
         # These can be used for predicting the mean for a new test-input
         # without inverting the covariance of the training points:
         self.predictive_weights = None
     
+    def kernel(self, x1, x2):
+        return self.cov_func(
+            x1, x2,
+            log_length=self.log_length,
+            log_scale=self.log_scale,
+            log_std=self.log_std
+        )
+    
     def predict(self, x_test):
         """See equations 6.66 and 6.67 in Bishop, 2006"""
+        # x_test should be an appropriate numpy.ndarray:
+        if type(x_test) is not np.ndarray: x_test = np.array(x_test)
+        if not len(x_test.shape) == 2: x_test = x_test.reshape(-1, 1)
+        # Invert the covariance matrix, if it has not been done so already:
         if not self.cov_train_inv:
             self.cov_train_inv = np.linalg.inv(self.cov_train)
-        k_predict = self.kernel(
-            x_test, self.x_train,
-            log_length=self.log_length, log_std=self.log_std
-        )
-        print(k_predict)
-        # mean = 
+        # Find the predictive weights, if they have not been found already:
+        if not self.predictive_weights:
+            self.predictive_weights = self.cov_train_inv.dot(self.targets)
+        # Find the covariance between train and test inputs:
+        k_predict = self.kernel(x_test, self.x_train)
+        # Calculate the predictive mean:
+        mean = k_predict.dot(self.predictive_weights)
+
+        # Find the covariance of the test inputs:
+        k_test = self.kernel(x_test, x_test)
+        # Calculate covariance and standard deviation:
+        cov = k_test - k_predict.dot(self.cov_train_inv).dot(k_predict.T)
+        # NB Floating point arithmetic can lead to negative variances
+        std = np.sqrt(np.maximum(np.diag(cov), 0)).reshape(mean.shape)
+
+        return mean, std
     
     def predict_mean(self, x_test):
         """Just predict the mean, if the standard deviation isn't needed"""
@@ -75,10 +94,8 @@ class GaussianProcess():
         if type(x_test) is not np.ndarray: x_test = np.array(x_test)
         if not len(x_test.shape) == 2: x_test = x_test.reshape(-1, 1)
 
-        k_predict = self.kernel(
-            x_test, self.x_train,
-            log_length=self.log_length, log_std=self.log_std
-        )
+        # Find the covariance between train and test inputs:
+        k_predict = self.kernel(x_test, self.x_train)
         # Fastest to use predictive weights, if they've been found:
         if self.predictive_weights:
             return k_predict.dot(self.predictive_weights)
@@ -86,7 +103,6 @@ class GaussianProcess():
         if self.cov_train_inv:
             return k_predict.dot(self.cov_train_inv).dot(self.targets)
         # Otherwise, find the predictive weights and predict the mean:
-        print("Evaluating predictive weights...")
         self.predictive_weights = np.linalg.solve(self.cov_train, self.targets)
 
         return k_predict.dot(self.predictive_weights)
